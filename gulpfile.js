367342fd401ef4c09e43577b9dac8787e1593fd3
Peter Sandford <peter@cameroncodes.com>
2021-02-04 15:19:13 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const terser = require('gulp-terser');
+
+const browserSync = require('browser-sync').create();
+
+function sassTask() {
+  return gulp.src("styles/**/*.scss")
+	  .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(gulp.dest('dist/css', { sourcemaps: '.' }));
+	  // .pipe(browserSync.stream());
+  }
+
+  function jsTask(){
+    return gulp.src('scripts/script.js', { sourcemaps: true })
+      .pipe(terser())
+      .pipe(gulp.dest('dist/scripts', { sourcemaps: '.' }));
+  }
+
+
+function browsersyncServe(cb){
+	browserSync.init({
+		server: {
+			baseDir:'.'
+		}
+	});
+	cb();
+}
+
+function browsersyncReload(cb){
+  browserSync.reload();
+  cb();
+}
+
+exports.default = gulp.series(
+  sassTask,
+  jsTask,
+  browsersyncServe,
+  watchTask
+);
+
+function watchTask(){
+  gulp.watch('*.html', browsersyncReload);
+  gulp.watch(['styles/**/*.scss', 'scripts/script.js'], gulp.series(sassTask, jsTask, browsersyncReload));
+}
+
+
+
