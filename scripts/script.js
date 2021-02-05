31c97c7c3ece580eee3db71da26067cbf96337f0
Peter Sandford <peter@cameroncodes.com>
2021-02-05 14:11:41 -0500

    adds homepage styles, images

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
-console.log("testing");
+"use strict"
+
+const today = new Date();
+const currentHour = today.getHours();
+
+if (currentHour < 12) {
+  document.getElementById("greeting").textContent = "Good morning!";
+} else if (currentHour < 18) {
+  document.getElementById("greeting").textContent = "Good afternoon!";
+} else {
+  document.getElementById("greeting").textContent = "Good evening!";
+}
